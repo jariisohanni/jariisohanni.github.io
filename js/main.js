0ab56f5ed9ef61238252fdd401ad2a0e662c5c7e
jariisohanni <jari.isohanni@gmail.com>
2021-11-14 19:47:22 +0200

    changes to file contents

--- js/main.js
+++ js/main.js
@@ -237,10 +237,30 @@ function imageFromFile(filename)
             blobFromBlobFile = base64toBlob(base64, MIMEType, 512);
             blobURL = URL.createObjectURL(blobFromBlobFile);
 
-            postImage(blobFromBlobFile,filename)
+            postImage(blobFromBlobFile,dataURItoBlob(filename))
         }
 
         reader.readAsBinaryString(filename);
       }
 
+}
+
+function dataURItoBlob(dataURI) {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(dataURI.split(',')[1]);
+  else
+      byteString = unescape(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], {type:mimeString});
 }
